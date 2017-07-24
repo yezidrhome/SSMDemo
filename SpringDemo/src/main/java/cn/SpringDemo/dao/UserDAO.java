1b161b1b9adc1422119a7a9f18030140aa91fc6d
yezi <yezi@DESKTOP-6DQ801M>
2017-07-24 19:42:05 +0800

    【完成页面显示全部用户】

---
+++ SpringDemo/src/main/java/cn/SpringDemo/dao/UserDAO.java
@@ -0,0 +1,23 @@
+package cn.SpringDemo.dao;
+
+import java.util.List;
+import cn.SpringDemo.model.User;
+
+/**
+ * 用户Dao层
+ * @author yezi
+ */
+public interface UserDAO {
+	/**
+	 * 添加一个用户
+	 * @param user
+	 * @return
+	 */
+	public int insertUser(User user);
+
+	/**
+	 * 获取全部用户 
+	 * @return
+	 */
+	public List<User> getAllUsers();
+}
